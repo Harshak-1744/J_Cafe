4dc60874758c3dec53c6f604379ee55787477fcb
Harsha <85007461+Harshak-1744@users.noreply.github.com>
2023-06-24 02:23:40 +0530

    Add files via upload

--- J_Cafe.java
+++ J_Cafe.java
@@ -5,14 +5,23 @@ public class J_Cafe {
         System.out.println("Aloah Welcome to J_Cafe");
         System.out.println("Menu:-");
 
-        String[] menu = new String[3];
+        String[][] menu = new String[3][3];
 
-        menu[0] = "1.Pizza";
-        menu[1] = "2.Burger";
-        menu[2] = "3.mocktailes";
+        menu[0][0] = "1. Pizza";
+        menu[0][1] = "2. Burger";
+        menu[0][2] = "3. mocktailes";
+        menu[1][0] = "4. coffee";
+        menu[1][1] = "5. Briyani";
+        menu[1][2] = "6. Pasteries";
+        menu[2][0] = "7. Noodles";
+        menu[2][1] = "8. FriedRice";
+        menu[2][2] = "9. Starters";
 
         for (int i = 0; i < menu.length; i++) {
-            System.out.println(menu[i]);
+            System.out.println();
+            for (int j = 0; j < menu[i].length; j++) {
+                System.out.print(menu[i][j] + " ");
+            }
         }
     }
 
